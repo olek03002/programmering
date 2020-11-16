1be4d5ca28d72e4ad63fc1a5aa7884ad349e7e99
olek03002 <74194646+olek03002@users.noreply.github.com>
2020-11-16 15:31:03 +0100

    Add files via upload

---
+++ ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int antal = 0;
+            Random slump = new Random();
+            int slumpen = slump.Next(0, 101);
+            Console.Write("Gissa slumptalet mellan 0 och 100: ");
+            int tal = Convert.ToInt32(Console.ReadLine());
+            antal++;
+            {
+                while (tal != slumpen)
+                {
+                    if (tal < slumpen)
+                    {
+                        Console.WriteLine("Talet är för lågt");
+                        Console.Write("Gissa igen: ");
+                        antal++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Talet är för högt");
+                        Console.Write("Gissa igen: ");
+                        antal++;
+                    }
+                    
+                }
+                Console.WriteLine("Rätt svar");
+                Console.WriteLine("Antal försök: " + antal);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
